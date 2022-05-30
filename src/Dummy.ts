3771bbcb589d0ef03bc64f3cfbba7b34710b860c
Tarun Samanta <tarunsamanta@Apples-MacBook-Pro-2.local>
2022-05-30 15:16:29 +0530

    jdche

--- src/Dummy.ts
+++ src/Dummy.ts
@@ -1,3 +1,14 @@
-export const dummyOutput = (input: string) => {
-    return input + input
+export const PrimeFacts = (n: number) : number[] => {
+     const factors = [];
+      let divisor = 2;
+
+      while (n >= 2) {
+        if (n % divisor == 0) {
+          factors.push(divisor);
+          n = n / divisor;
+        } else {
+          divisor++;
+        }
+      }
+      return factors;
 }
--- test/src/Dummy.test.ts
+++ test/src/Dummy.test.ts
@@ -1,8 +1,27 @@
-import { dummyOutput } from '../../src/Dummy'
+import { PrimeFacts } from '../../src/Dummy'
 
-describe("Dummy Test", () => {
-    it("should assert on the dummy output", () => {
-        const dummyOutputReturned = dummyOutput("hello")
-        expect(dummyOutputReturned).toBe("hellohello")
+describe("Prime Factorization", () => {
+    it("should be same as prime factors of 2 ", () => {
+        const dummyOutputReturned: number[] = PrimeFacts(2)
+        expect(dummyOutputReturned).toEqual([2])
     })
+    it("should be same as prime factors of 4 ", () => {
+            const dummyOutputReturned: number[] = PrimeFacts(4)
+            expect(dummyOutputReturned).toEqual([2,2])
+        })
+
+     it("should be same as prime factors of 8 ", () => {
+                const dummyOutputReturned: number[] = PrimeFacts(8)
+                expect(dummyOutputReturned).toEqual([2,2,2])
+     })
+
+     it("should be same as prime factors of 25 ", () => {
+                     const dummyOutputReturned: number[] = PrimeFacts(25)
+                     expect(dummyOutputReturned).toEqual([5,5])
+     })
+
+     it("should be same as prime factors of 14 ", () => {
+                          const dummyOutputReturned: number[] = PrimeFacts(14)
+                          expect(dummyOutputReturned).toEqual([2,7])
+          })
 })
